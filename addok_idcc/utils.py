bc34d00f9e69e8eaff0f8ff811133a4a2e97b401
Yohan Boniface <yb@enix.org>
2018-07-20 13:56:05 +0200

    Do not import codes 9999 and 9998, which means "no idcc"

--- addok_idcc/utils.py
+++ addok_idcc/utils.py
@@ -2,6 +2,14 @@ from addok.config import config
 from addok.http.base import Search as BaseSearch
 
 
+def filter_documents(docs):
+    for doc in docs:
+        # Those means "no IDCC".
+        if doc['code'] in ['9999', '9998']:
+            continue
+        yield doc
+
+
 def format_result(result):
     return {field['key']: getattr(result, field['key'])
             for field in config.FIELDS}
